a0d0131cd1e7b75508fba894340cf22eb1fa5d39
ryo <ryo12271443@icloud.com>
2020-05-29 00:15:10 +0900

    タイトルバー以外は完成

--- kadai_03/app/src/main/java/kadai_03/com/MainActivity.kt
+++ kadai_03/app/src/main/java/kadai_03/com/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +14,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        RUN_button.setOnClickListener{
-            textView.text="KKK"
+        RUN_button.setOnClickListener(){
+            val a = Cul()
+            val year:Int?=textbox.getText().toString().toIntOrNull()
+            textView.text=a.isLeap(year)
         }
+
 
     }
 
+}
+
+
+class Cul{
+
+    fun isLeap(year:Int?):String{
+
+        if(year !is Int || year <= 0 || year == null){
+            return "please enter nutural number"
+        }
+        //自然数じゃないとき
+
+        if(year % 4 == 0){
+            if(year % 400==0) return "${year} is leapyear!"
+
+            if(year % 100 ==0) return "${year} is not leapyear!"
+
+            return "${year} is leapyear!"
+
+
+        }else{
+            return "${year} is not leapyear!"
+        }
+        //うるう年判定
+    }
 }
