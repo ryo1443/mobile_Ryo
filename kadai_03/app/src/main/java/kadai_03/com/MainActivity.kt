b9348186bc0a53ea8729e3b721792333cf290efd
ryo <ryo12271443@icloud.com>
2020-05-28 22:39:09 +0900

    ボタンアクションの追加

--- kadai_03/app/src/main/java/kadai_03/com/MainActivity.kt
+++ kadai_03/app/src/main/java/kadai_03/com/MainActivity.kt
@@ -2,11 +2,21 @@ package kadai_03.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        RUN_button.setOnClickListener{
+            textView.text="KKK"
+        }
+
     }
+
 }
