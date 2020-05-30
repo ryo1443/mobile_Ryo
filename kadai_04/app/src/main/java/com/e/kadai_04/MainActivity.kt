b11e757d8ac4a6df731995a78c3f744c8eceeb4a
ryo <ryo12271443@icloud.com>
2020-05-30 14:32:06 +0900

    レイアウトの大まかな完成。

--- kadai_04/app/src/main/java/com/e/kadai_04/MainActivity.kt
+++ kadai_04/app/src/main/java/com/e/kadai_04/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.e.kadai_04
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    fun btn(v:View){
+        when(v.getId()){
+            Paper_btn.id->textView.text="Paper"
+            Rock_btn.id->textView.text="Rock"
+            Scissorce_btn.id->textView.text="Scissorce"
+
+        }
+    }
+
+
+}
+
+enum class Hand{
+    Rock,
+    Paper,
+    Scissorce
+}
+
+enum class result{
+    win,
+    lose
+}
+class btn_input(val name:Hand){
+
 }
