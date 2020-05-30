58d4a185a76114bcf40f191918af48dcc1591ac7
ryo <ryo12271443@icloud.com>
2020-05-31 01:20:08 +0900

    じゃんけんメソッドの追加。

--- kadai_04/app/src/main/java/com/e/kadai_04/MainActivity.kt
+++ kadai_04/app/src/main/java/com/e/kadai_04/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.e.kadai_04
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,30 +11,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    }
-
-    fun btn(v:View){
-        when(v.getId()){
-            Paper_btn.id->textView.text="Paper"
-            Rock_btn.id->textView.text="Rock"
-            Scissorce_btn.id->textView.text="Scissorce"
-
+        Paper_btn.setOnClickListener {
+            janken(Hand.Paper)
+        }
+        Rock_btn.setOnClickListener {
+            janken(Hand.Rock)
+        }
+        Scissors_btn.setOnClickListener {
+            janken(Hand.Scissors)
         }
     }
 
+    fun janken(HandType:Hand){
+        val cp =(0..2).random()
+        var CP:Hand?=null
 
+        when(cp){
+            0->{CP=Hand.Rock}
+            1->{CP=Hand.Scissors}
+            2->{CP=Hand.Paper}
+        }
+        CP?.let{
+            if(HandType==it){
+                textView.text="You ${Result.Draw}! cp is $it" //あいこ
+            }else if((HandType==Hand.Rock && it==Hand.Scissors)||(HandType==Hand.Scissors && it==Hand.Paper)||(HandType==Hand.Paper && it==Hand.Rock)){
+                textView.text="You ${Result.Win}! cp is $it"  //勝ち
+            }else{
+                textView.text="You ${Result.Lose}! cp is $it"  //負け
+            }
+        }
+        if(CP==null) textView.text="Error"
+    }
 }
-
 enum class Hand{
     Rock,
     Paper,
-    Scissorce
+    Scissors
 }
 
-enum class result{
-    win,
-    lose
+enum class Result{
+    Win,
+    Lose,
+    Draw
 }
-class btn_input(val name:Hand){
-
+class Test:AppCompatActivity(){
+    fun test(){
+        textView.text="test"
+    }
 }
